e10678d19db4316d2df0538dd2c0520259ce6a63
itspebbleboy <itspebbleboy@gmail.com>
2023-06-05 11:40:12 -0700

    eye function psuedo code

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -7,7 +7,6 @@ class Play extends Phaser.Scene {
   preload(){
     //state machine for eye orientation
     //load images
-    
   }
 
   create(){
@@ -28,8 +27,20 @@ class Play extends Phaser.Scene {
 
   update(){
     //player's location, cardinal direction, & image display
-
-    
   }
 
+  moveEyeRight(){
+    //moves eye right
+    //from state left -> forward
+    //or state forward -> right
+  }
+  moveEyeLeft(){
+    //moves eye left
+    //from state right -> forward
+    //or state forward -> left
+  }
+  changeCardinalDirection(currCardDirection, leftOrRIght){
+    //given the current cardinal direction & if the movement is towards the left or the right
+    //change the player's curr cardinal direction to be facing left or right
+  }
 }
