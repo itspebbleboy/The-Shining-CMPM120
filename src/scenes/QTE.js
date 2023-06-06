7f1d3a4de8c91b7c5d043808d4f067016a412467
itspebbleboy <itspebbleboy@gmail.com>
2023-06-05 17:46:42 -0700

    Update QTE.js

--- src/scenes/QTE.js
+++ src/scenes/QTE.js
@@ -1,113 +1,108 @@
 class QTE extends Phaser.Scene {
-    constructor() {
-      super("qteScene");
-      this.qteInProgress = false;
-      this.dialogueList = [];
-      this.currentTextIndex = 0;
-      this.fastForward = false;
-      this.currentIndex = 0;
-      this.waitForSpace = false;
-      this.textCrawl = null;
-    }
+  constructor() {
+    super("qteScene");
+    this.qteInProgress = false;
+    this.dialogueList = [];
+    this.currentTextIndex = 0;
+    this.fastForward = false;
+    this.currentIndex = 0;
+    this.textCrawlActive = false; // Flag to track the text crawl state
+    this.qteInputOptions = ['a', 'g', 'i', 'k', 'm', 'b', 'r', 'p']; // QTE input options
+    this.currentQTEInputOption = null; // Current QTE input option
+  }
+
+  preload() {
+    this.dialogueList = [
+      "First dialogue",
+      "Second dialogue",
+      "Third dialogue",
+      // Add more dialogues as needed
+    ];
+  }
+
+  create() {
+    this.textCrawlSpeed = 100; // Adjust the speed of the text crawl (time between each character)
   
-    preload() {
-      this.dialogueList = [
-        "First dialogue",
-        "Second dialogue",
-        "Third dialogue",
-        // Add more dialogues as needed
-      ];
-    }
+    this.startNextDialogue();
   
-    create() {
-      const textCrawlSpeed = 100; // Adjust the speed of the text crawl (time between each character)
-  
-      const addCharacter = (textCrawlText) => {
-        this.currentTextIndex++;
-        this.textCrawl.text = textCrawlText.substring(0, this.currentTextIndex);
-  
-        if (this.currentTextIndex < textCrawlText.length) {
-          const delay = this.fastForward ? 0 : textCrawlSpeed;
-          this.time.delayedCall(delay, addCharacter, [textCrawlText]);
-        } else {
-          if (this.currentIndex < this.dialogueList.length - 1) {
-            this.currentIndex++;
-            this.currentTextIndex = 0;
-            const nextTextCrawlText = this.dialogueList[this.currentIndex];
-            addCharacter(nextTextCrawlText);
-          } else {
-            this.waitForSpace = true;
-          }
-        }
-      };
-  
-      const startNextDialogue = () => {
-        console.log("starting next ")
-        if (this.currentIndex >= this.dialogueList.length) {
+    // Add event listener for the spacebar key during text crawl and QTE
+    this.input.keyboard.on("keydown-SPACE", () => {
+      if (!this.textCrawlActive) {
+        // If the text crawl is not active, proceed to the next dialogue when spacebar is pressed
+        if (this.currentIndex === this.dialogueList.length - 1) {
+          // Last dialogue, start QTE
+          this.textCrawl.destroy();
           this.startQTE();
-          return;
+        } else {
+          this.currentIndex++;
+          this.textCrawl.destroy();
+          this.startNextDialogue();
+          console.log("Proceeding to next dialogue");
         }
-  
-        const dialogue = this.dialogueList[this.currentIndex];
-  
-        this.textCrawl = this.add.text(screen.botMid.x, screen.botMid.y - 20, "", defaultTextCrawlStyle);
-  
-        this.textCrawl.setOrigin(0, 1);
-        this.textCrawl.setAlign("center");
-  
-        this.currentTextIndex = 0;
-        this.waitForSpace = false;
-  
-        addCharacter(dialogue);
-      };
-  
-      startNextDialogue();
-  
-      // Add event listener for the spacebar key during text crawl
-      this.input.keyboard.on("keydown-SPACE", () => {
-        if (!this.waitForSpace) {
-            if (this.currentIndex === this.dialogueList.length - 1) {
-                // Last dialogue, start QTE
-                this.startQTE();
-            }else {
-            // Proceed to the next dialogue
-                this.currentIndex++;
-                this.textCrawl.destroy();
-                startNextDialogue();
-            }
-        }
-      });
-  
-      // Add event listener for the spacebar key during QTE
-      this.input.keyboard.on("keydown-SPACE", this.handleQTEInput, this);
-  
-      // Enable fast forward on shift keydown
-      this.input.keyboard.on("keydown-SHIFT", () => {
-        this.fastForward = true;
-      });
-  
-      // Disable fast forward on shift keyup
-      this.input.keyboard.on("keyup-SHIFT", () => {
-        this.fastForward = false;
-      });
-    }
-  
-    // ...
-  
-    startQTE() {
-      // QTE logic goes here
-      // display text
-      this.qteText = this.add.text(screen.topMid.x, screen.topMid.y, "Press SPACE!", defaultQTEStyle);
-      this.qteInProgress = true; // Set the QTE in progress flag to true
-      console.log("QTE started!");
-    }
-  
-    handleQTEInput() {
-      // QTE input handling goes here
-      if (this.qteInProgress) {
-        console.log("QTE input detected!");
-        this.qteInProgress = false;
       }
+    });
+    // Add event listener for the Shift key to enable fast forward
+    this.input.keyboard.on("keydown-SHIFT", () => {
+      this.fastForward = true;
+    });
+
+    // Add event listener for releasing the Shift key to disable fast forward
+    this.input.keyboard.on("keyup-SHIFT", () => {
+      this.fastForward = false;
+    });
+  }
+
+  startQTE() {
+    // QTE logic goes here
+    // Generate a new random QTE input option
+    this.currentQTEInputOption = this.qteInputOptions[Phaser.Math.Between(0, this.qteInputOptions.length - 1)];
+
+    // Display text
+    this.qteText = this.add.text(screen.topMid.x, screen.topMid.y, "Press " + this.currentQTEInputOption.toUpperCase() + "!", defaultQTEStyle);
+    this.qteInProgress = true; // Set the QTE in progress flag to true
+    console.log("QTE started!");
+
+    // Update event listener for the new QTE input option
+    this.input.keyboard.removeAllListeners(); // Remove all previous event listeners
+    this.input.keyboard.on("keydown-" + this.currentQTEInputOption.toUpperCase(), this.handleQTEInput, this);
+  }
+
+  handleQTEInput(event) {
+    // QTE input handling goes here
+    if (this.qteInProgress && event.key === this.currentQTEInputOption) {
+      console.log("QTE input handled!");
+      this.qteInProgress = false;
     }
   }
+
+  addCharacter(dialogue) {
+    this.currentTextIndex++;
+    this.textCrawl.text = dialogue.substring(0, this.currentTextIndex);
+
+    if (this.currentTextIndex < dialogue.length) {
+      const delay = this.fastForward ? 0 : this.textCrawlSpeed;
+      this.time.delayedCall(delay, () => this.addCharacter(dialogue));
+    } else {
+      this.textCrawlActive = false; // Set the text crawl state to inactive
+      console.log("setting textCrawlActive false");
+    }
+  }
+
+  startNextDialogue() {
+    if (this.currentIndex >= this.dialogueList.length) {
+      // All dialogues completed, exit or do something else
+      return;
+    }
   
+    const dialogue = this.dialogueList[this.currentIndex];
+  
+    this.textCrawl = this.add.text(screen.botMid.x, screen.botMid.y - 20, "", defaultTextCrawlStyle);
+    this.textCrawl.setOrigin(0, 1);
+    this.textCrawl.setAlign("center");
+  
+    this.currentTextIndex = 0;
+    this.textCrawlActive = true; // Set the text crawl state to active
+  
+    this.addCharacter(dialogue);
+  }
+}
