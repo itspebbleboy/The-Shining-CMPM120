f91a961b5e7c2f2b54511e6b69916eb558e52738
itspebbleboy <itspebbleboy@gmail.com>
2023-06-05 18:02:29 -0700

    added QTE failure, & textbox ui into preload

--- src/scenes/QTE.js
+++ src/scenes/QTE.js
@@ -9,8 +9,11 @@ class QTE extends Phaser.Scene {
     this.textCrawlActive = false; // Flag to track the text crawl state
     this.qteInputOptions = ['a', 'g', 'i', 'k', 'm', 'b', 'r', 'p']; // QTE input options
     this.currentQTEInputOption = null; // Current QTE input option
+    this.qteCount = 3; // Number of QTEs
+    this.completedQTEs = 0; // Number of completed QTEs
+    this.qteTimer = null; // Timer for QTE
+    this.qteTimerDuration = 5000; // Duration of the QTE timer in milliseconds (5 seconds)
   }
-
   preload() {
     this.dialogueList = [
       "First dialogue",
@@ -18,11 +21,13 @@ class QTE extends Phaser.Scene {
       "Third dialogue",
       // Add more dialogues as needed
     ];
+
+    this.load.image('textBox', './assets/ui/textBox.png')
   }
 
   create() {
     this.textCrawlSpeed = 100; // Adjust the speed of the text crawl (time between each character)
-  
+    this.add.image(screen.center.x,screen.center.y,)
     this.startNextDialogue();
   
     // Add event listener for the spacebar key during text crawl and QTE
@@ -53,7 +58,6 @@ class QTE extends Phaser.Scene {
   }
 
   startQTE() {
-    // QTE logic goes here
     // Generate a new random QTE input option
     this.currentQTEInputOption = this.qteInputOptions[Phaser.Math.Between(0, this.qteInputOptions.length - 1)];
 
@@ -61,6 +65,10 @@ class QTE extends Phaser.Scene {
     this.qteText = this.add.text(screen.topMid.x, screen.topMid.y, "Press " + this.currentQTEInputOption.toUpperCase() + "!", defaultQTEStyle);
     this.qteInProgress = true; // Set the QTE in progress flag to true
     console.log("QTE started!");
+
+    // Set up the timer for the QTE
+    this.qteTimer = this.time.delayedCall(this.qteTimerDuration, this.handleQTEFailure, [], this);
+    console.log("QTE timer started!");
 
     // Update event listener for the new QTE input option
     this.input.keyboard.removeAllListeners(); // Remove all previous event listeners
@@ -72,8 +80,43 @@ class QTE extends Phaser.Scene {
     if (this.qteInProgress && event.key === this.currentQTEInputOption) {
       console.log("QTE input handled!");
       this.qteInProgress = false;
+      this.qteTimer.remove(); // Remove the QTE timer
+      this.qteText.destroy(); // Destroy the current QTE text
+
+      this.completedQTEs++;
+
+      if (this.completedQTEs < this.qteCount) {
+        // Start the next QTE
+        this.startQTE();
+      } else {
+        // All QTEs completed, do something else
+        console.log("All QTEs completed");
+      }
     }
   }
+
+  handleQTEFailure() {
+    console.log("QTE failure!");
+
+    this.qteInProgress = false;
+    this.qteText.destroy(); // Destroy the current QTE text
+
+    // Display failure message
+    this.failureText = this.add.text(screen.topMid.x, screen.topMid.y, "You were too slow, you lost!", defaultQTEStyle);
+    this.time.delayedCall(2000, () => {
+      this.failureText.destroy();
+      
+      if (this.completedQTEs < this.qteCount) {
+        // Start the next QTE
+        this.startQTE();
+      } else {
+        // All QTEs completed, do something else
+        console.log("All QTEs completed");
+      }
+    });
+  }
+
+  // ...
 
   addCharacter(dialogue) {
     this.currentTextIndex++;
