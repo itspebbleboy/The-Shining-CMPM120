b33922625b2fb269b4777213892f8d98485cffe0
Athena Patronas <97550706+pyromaniac3@users.noreply.github.com>
2023-06-10 15:23:27 -0700

    menu theme

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -18,6 +18,7 @@ class Menu extends Phaser.Scene {
         keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
         //#endregion
         this.load.image('brownBackground', './assets/ui/brownBackground.png');
+        this.scene.load.audio('theme', './assets/music/Theme.mp3');
         
     }
 
