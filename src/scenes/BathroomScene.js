8a3c876265fa7ff44773c9e7b7cbf39f6e340085
Athena Patronas <97550706+pyromaniac3@users.noreply.github.com>
2023-06-10 13:09:56 -0700

    animation for axe in bathroom

--- src/scenes/BathroomScene.js
+++ src/scenes/BathroomScene.js
@@ -22,10 +22,119 @@ class BathroomScene extends Phaser.Scene {
         this.load.image('closeWindow','./assets/cutscene/closeWindow.png');
 
     }create(){
+        this.anims.create({
+            key: 'axeScene',
+            frames: [
+                {key: 'bathroom0', frame:null},
+                {key: 'bathroom1', frame:null},
+                {key: 'bathroom2', frame:null},
+                {key: 'bathroom3', frame:null},
+                {key: 'bathroom4', frame:null},
+                {key: 'bathroom5', frame:null}
+            ],
+            frameRate: 1,
+          });
         
     }update(){
 
     }
-    sceneFinished(){
+    cutscenePartTwo= () =>{
+        //DORKNOB AUDIO
+        this.cutsceneHelper.iterateThroughDialogue(
+            [
+                "oh god,",
+                "he's right outside!",
+            ], this.cutscenePartThree, this);
+    }
+    cutscenePartThree= () =>{
+        this.cutsceneHelper.createBlinkingText("GRAB YOUR SON", 2000, this);
+        this.cutsceneHelper.startQTE(3, this.cutscenePartFour, this);
+    }
+    cutscenePartFour= () =>{
+        this.background.destroy();
+        this.background = this.add.image(screen.center.x,screen.center.y, 'background').setOrigin(0.5,0.5);
+        this.cutsceneHelper.iterateThroughDialogue(
+            [
+                "he made it into the room, but you've locked yourself and your son in the bathroom.",
+                "you've bought yourself some time but he's going to eventually get in here.",
+            ], this.cutscenePartFive, this);
+    }
+    cutscenePartFive= () =>{
+        this.background.destroy();
+        this.background = this.add.image(screen.center.x,screen.center.y, 'closeWindow').setOrigin(0.5,0.5); //REPLACE WITH EMPTY WINDOW
+        this.cutsceneHelper.iterateThroughDialogue(
+            [
+                "wait,",
+                "the window!",
+            ], this.cutscenePartSix, this);
+    }
+    cutscenePartSix= () =>{
+        this.cutsceneHelper.createBlinkingText("PUSH YOUR SON THROUGH", 2000, this);
+        this.cutsceneHelper.startQTE(2, this.cutscenePartSeven, this);
+    }
+    cutscenePartSeven= () =>{
+        this.background.destroy();
+        this.background = this.add.image(screen.center.x,screen.center.y, 'wendyWindow').setOrigin(0.5,0.5);
+        this.cutsceneHelper.iterateThroughDialogue(
+            [
+                "now,",
+                "he made it out but",
+                "you still have to escape",
+            ], this.cutscenePartEight, this);
+    }
+    cutscenePartEight =() =>{
+        this.background.destroy();
+        this.background = this.add.image(screen.center.x,screen.center.y, 'openWindow').setOrigin(0.5,0.5); //REPLACE WITH EMPTY WINDOW
+        this.cutsceneHelper.createBlinkingText("ESCAPE THROUGH THE WINDOW", 2000, this);
+        this.cutsceneHelper.startQTE(2, this.cutscenePartNine, this);
+    }
+    cutscenePartNine = () =>{
+        this.background.destroy();
+        this.background = this.add.image(screen.center.x,screen.center.y, 'wendyWindow').setOrigin(0.5,0.5);
+        this.cutsceneHelper.iterateThroughDialogue(
+            [
+                "you didn't fit through",
+                "you told your son to run",
+                "you're not sure if you're going to survive this",
+            ], this.cutscenePartTen, this);
+    }
+    cutscenePartTen = () =>{
+        this.background.destroy();
+        this.background = this.add.image(screen.center.x,screen.center.y, 'background').setOrigin(0.5,0.5); //THIS ONE STAYS BACKGROUND
+        //PLAY AXE TO DOOR AUDIO
+        this.cutsceneHelper.createBlinkingText("MOVE TO THE CORNER", 2000, this);
+        this.cutsceneHelper.startQTE(2, this.cutscenePartEleven, this);
+    }
+    cutscenePartEleven = () =>{
+        this.background.destroy();
+        this.background = this.add.image(screen.center.x,screen.center.y, 'bathroom0').setOrigin(0.5,0.5);
+        this.cutsceneHelper.iterateThroughDialogue(
+            [
+                "that was close,",
+                "but it isn't over",
+            ], this.cutscenePartTwelve, this);
+    }
+    cutscenePartTwelve = () =>{
+        //PLAY ANIMATION
+        //on animation complete, play QTE
+        this.add.sprite(screen.center.x,screen.center.y).play('axeScene'); // play axe
+        this.time.delayedCall(5000, ()=>{
+            this.cutsceneHelper.createBlinkingText("USE THE KNIFE", 2000, this);
+            this.cutsceneHelper.startQTE(5, this.cutscenePartThirteen, this);
+          }, [], this);
+    }
+    cutscenePartThirteen = () =>{
+        this.background.destroy();
+        this.background = this.add.image(screen.center.x,screen.center.y, 'background').setOrigin(0.5,0.5);
+        this.cutsceneHelper.iterateThroughDialogue(
+            [
+                "it's cold",
+                "your mother...",
+            ], this.cutscenePartTwelve, this);
+    }
+    startScene= () =>{
+        this.background.destroy();
+        this.background = this.add.image(screen.center.x,screen.center.y, 'dannyWindow').setOrigin(0.5,0.5);
+
     }
 }
